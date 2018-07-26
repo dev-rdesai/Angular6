372b5599f372b6186dd085f5093c0b3a8ff2b5f4
Ravalnath Desai <ravalnath.d@gmail.com>
2018-07-26 03:15:55 -0400

    Update my.module.js

--- src/my.module.js
+++ src/my.module.js
@@ -16,7 +16,7 @@ var MyComponent = (function () {
 MyComponent = __decorate([
     core_1.Component({
         selector: 'my-component',
-        templateUrl: './my.module.html'
+        templateUrl: 'my.module.html'
     })
 ], MyComponent);
 exports.MyComponent = MyComponent;
