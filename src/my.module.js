db575ed71e0196ccd8bdcd6f9cddf83f5651a339
Ravalnath Desai <ravalnath.d@gmail.com>
2018-07-26 03:47:08 -0400

    Update my.module.js

--- src/my.module.js
+++ src/my.module.js
@@ -15,10 +15,10 @@ var MyComponent = (function () {
 }());
 MyComponent = __decorate([
     core_1.Component({
+        encapsulation: core_1.ViewEncapsulation.None,
         selector: 'my-component',
         templateUrl: 'https://raw.githubusercontent.com/ravalnath/Angular6/master/src/my.module.html',
         styleUrls:['https://raw.githubusercontent.com/ravalnath/Angular6/master/src/module.css']
-        
     })
 ], MyComponent);
 exports.MyComponent = MyComponent;
